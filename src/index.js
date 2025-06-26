23aef6e95ade08cc5c369b9f0bc883ea05fd2904
Ignas <15948796+ignasc@users.noreply.github.com>
2025-06-26 16:03:28 +0100

    Add page tab as seperate JS file.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import "./resetcss.css";
 import "./styles.css";
 
 import {pageLoadedMessage, functionMessage} from "./pageLoad.js";
+import {tabName, header, contents} from "./tab-home.js";
 
 console.log(pageLoadedMessage);
+
+const contentElement = document.querySelector("#content");
+
+contentElement.appendChild(tabName);
+contentElement.appendChild(header);
+contentElement.appendChild(contents);
 
 functionMessage();
---
+++ src/tab-home.js
@@ -0,0 +1,9 @@
+const tabName = document.createElement("h1");
+const header = document.createElement("h2");
+const contents = document.createElement("p");
+
+tabName.textContent = "Home";
+header.textContent = "The Restaurant";
+contents.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus inventore delectus impedit! Blanditiis eius architecto consectetur exercitationem officiis est repellat aperiam distinctio. Quidem qui deleniti nemo atque, beatae id praesentium.";
+
+export {tabName, header, contents};
