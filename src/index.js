fb47c7fe67e1dee7ac582feaefd9e83c69908c34
Ignas <15948796+ignasc@users.noreply.github.com>
2025-07-02 16:40:28 +0100

    Move tab switching logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import "./resetcss.css";
 import "./styles.css";
 
-import {btn_home, btn_menu, btn_contacts, btn_about} from "./navigation.js";
+import showHomeContent from "./tab-home";
+import showMenuContent from "./tab-menu";
+import showContactsContent from "./tab-contacts";
+import showAboutContent from "./tab-about";
+
+//import {btn_home, btn_menu, btn_contacts, btn_about} from "./navigation.js";
 
 const navigationMenu = document.querySelector("nav");
 
-navigationMenu.appendChild(btn_home);
-navigationMenu.appendChild(btn_menu);
-navigationMenu.appendChild(btn_contacts);
-navigationMenu.appendChild(btn_about);
+const allContent = {
+    "btn-home": showHomeContent,
+    "btn-menu": showMenuContent,
+    "btn-contacts": showContactsContent,
+    "btn-about": showAboutContent,
+};
+
+const btn_all = document.querySelectorAll("nav > button");
+
+for (let index = 0; index < btn_all.length; index++) {
+    const element = btn_all[index];
+
+    element.addEventListener("click", (e)=>{
+        loadPageContent(e.target.id);
+        setActiveTab(e.target.id);
+    });
+}
+
+function loadPageContent(id){
+    allContent[id]();
+};
+
+function setActiveTab(id){
+    for (let index = 0; index < btn_all.length; index++) {
+        const element = btn_all[index];
+        
+        if (element.id == id) {
+            element.style.backgroundColor = "var(--tab-active-color)";
+        } else {
+            element.style.backgroundColor = "var(--main-bg-color)";
+        }
+    }
+};
+
 
 //trigger home button to show home page on first page load
-btn_home.click();
+btn_all[0].click();
